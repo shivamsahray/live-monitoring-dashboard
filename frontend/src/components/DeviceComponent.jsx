400f987c81ed364c69e8fc5462eccc28aec5ff6b
shivamsahray <sahrayshivam@gmail.com>
2025-07-10 17:42:13 +0530

    Added DeviceCard

---
+++ frontend/src/components/DeviceComponent.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useNavigate } from "react-router-dom"
+
+
+const DeviceCard = ({device}) => {
+    const navigate = useNavigate();
+
+    return (
+        <>
+        <div className="border p-4 bg-red-100 rounded" >
+            <h2 className="font-bold mb-1">{device.name}</h2>
+            <div className="bg-white p-2 mb-1">Line 1: UNKNOWN | Voltage: N/A</div>
+            <div className="bg-white p-2 mb-1">Line 2: UNKNOWN | Voltage: N/A</div>
+            <div className="bg-white p-2 mb-1">Line 3: UNKNOWN | Voltage: N/A</div>
+            <div className="bg-white p-2 mb-1">Line 4: UNKNOWN | Channel: N/A</div>
+
+            <button className="bg-blue-500 text-white px-3 py-1 rounded">Recordings</button>
+        </div>
+        </>
+    )
+}
+
+export default DeviceCard;
